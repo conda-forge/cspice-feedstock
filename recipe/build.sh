1ebdcc68fb3c4712d9de327002e1ee1395d30b6e
Andrew Annex <ama6fy@virginia.edu>
2020-11-27 16:53:15 -0500

    added rebuilding of shared library from source for *NIX platforms, this is a major change in how this recipe works

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,24 +9,29 @@ else
     EXTRA_FLAGS="-shared -Wl,-soname,${LIBNAME}"
 fi
 
+# Build and Package dynamic libraries
+#  cd to lib dir
+cd ${SRC_DIR}/lib
+#  compile c code
+${CC} -Iinclude -c -fPIC -m64 -O2 -ansi -pedantic ./../src/cspice/*.c ./../src/csupport/*.c 
+#  make the shared library
+${CC} ${EXTRA_FLAGS} -fPIC -m64 -O2 -pedantic -o ${LIBNAME} *.o -lm
+#  cd up to src dirctory
+cd ${SRC_DIR} 
+
+# todo add rebuilding of all executeables
+
+# Deploy the built shared libraries and executables
+#  make the target directories
 mkdir -p ${PREFIX}/include/cspice
 mkdir -p ${PREFIX}/lib
 mkdir -p ${PREFIX}/bin
-
-# Copy the binaries
+#  copy the files to where they are needed
 cp $(find $(find ${SRC_DIR} -name "exe" -type d) -type f) ${PREFIX}/bin
-
-# Package dynamic libraries
-cd $(find ${SRC_DIR} -name "lib" -type d)
-
-ar -x cspice.a
-${CC} ${EXTRA_FLAGS} -fPIC -lm *.o -o ${LIBNAME}
-
-cd $(find ${SRC_DIR} -name "lib" -type d)/..
-
 cp lib/${LIBNAME} ${PREFIX}/lib/
 cp include/*.h ${PREFIX}/include/cspice/
 
+# finally copy the dylib or so file to where it is needed
 if [ "$(uname)" == "Darwin" ];
 then
     ln -s ${PREFIX}/lib/${LIBNAME} ${PREFIX}/lib/libcspice.dylib
