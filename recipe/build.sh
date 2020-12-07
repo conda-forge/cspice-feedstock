7abad2e0a464c7131d94b1a8825b3f750608b4d4
Andrew Annex <ama6fy@virginia.edu>
2020-12-07 17:51:59 -0500

    Make nix build everything from src (#22)
    * added rebuilding of shared library from source for *NIX platforms, this is a major change in how this recipe works
    
    * MNT: Re-rendered with conda-build 3.20.5, conda-smithy 3.8.5, and conda-forge-pinning 2020.11.26.07.13.25
    
    * removed csupport source from cspice shared library
    
    * builds shared library and static library from scratch
    
    * being more explicit with copying libraries
    
    Co-authored-by: conda-forge-linter <github-actions@email.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,28 +8,76 @@ else
     LIBNAME=libcspice.so.66
     EXTRA_FLAGS="-shared -Wl,-soname,${LIBNAME}"
 fi
+# static library files names
+CSPICENM=cspice.66.a
+CSUPPTNM=csupport.66.a
 
+#########################################
+# Build Shared library
+#########################################
+#  cd to lib dir
+cd ${SRC_DIR}/lib
+#  rm static libraries, needed for next static build steps
+rm cspice.a
+rm csupport.a
+#  compile c code
+${CC} -Iinclude -c -fPIC -m64 -O2 -ansi -pedantic ./../src/cspice/*.c 
+#  make the shared library
+${CC} ${EXTRA_FLAGS} -fPIC -m64 -O2 -pedantic -o ${LIBNAME} *.o -lm
+#  cd up to src directory
+cd ${SRC_DIR}
+
+#########################################
+# Build Static library using NAIF scripts
+#########################################
+#  rebuild static library using NAIF scripts
+export TKCOMPILER=${CC}
+cd ${SRC_DIR}/src/cspice
+${BUILD_PREFIX}/bin/tcsh ./mkprodct.csh
+cd ${SRC_DIR}/src/csupport
+${BUILD_PREFIX}/bin/tcsh ./mkprodct.csh
+#  rename static libraries to include version number
+cd ${SRC_DIR}/lib
+cp cspice.a ${CSPICENM}
+cp csupport.a ${CSUPPTNM}
+#  cd up to src directory
+cd ${SRC_DIR}
+
+#########################################
+# Build executables using NAIF scripts
+#########################################
+# rm all prebuilt executables to ensure we are building and testing new files
+rm ${SRC_DIR}/exe/*
+# cd into src directory
+cd ${SRC_DIR}/src
+# build each tool using NAIF scripts
+for i in *_c; do cd $i && ${BUILD_PREFIX}/bin/tcsh ./mkprodct.csh && cd -; done
+#  cd up to src directory
+cd ${SRC_DIR}
+#  remove cspice.a and csupport.a as we want those to be symlinked, don't do this earlier for tool building
+rm ${SRC_DIR}/lib/cspice.a
+rm ${SRC_DIR}/lib/csupport.a
+
+#########################################
+# deploy built products
+#########################################
+# Deploy the built shared libraries and executables
+#  make the target directories
 mkdir -p ${PREFIX}/include/cspice
 mkdir -p ${PREFIX}/lib
 mkdir -p ${PREFIX}/bin
-
-# Copy the binaries
+#  copy the files to where they are needed
 cp $(find $(find ${SRC_DIR} -name "exe" -type d) -type f) ${PREFIX}/bin
-
-# Package dynamic libraries
-cd $(find ${SRC_DIR} -name "lib" -type d)
-
-ar -x cspice.a
-${CC} ${EXTRA_FLAGS} -fPIC -lm *.o -o ${LIBNAME}
-
-cd $(find ${SRC_DIR} -name "lib" -type d)/..
-
 cp lib/${LIBNAME} ${PREFIX}/lib/
+cp lib/${CSPICENM} ${PREFIX}/lib/
+cp lib/${CSUPPTNM} ${PREFIX}/lib/
 cp include/*.h ${PREFIX}/include/cspice/
-
+#  finally make symbolic links for sans version file names
 if [ "$(uname)" == "Darwin" ];
 then
     ln -s ${PREFIX}/lib/${LIBNAME} ${PREFIX}/lib/libcspice.dylib
 else
     ln -s ${PREFIX}/lib/${LIBNAME} ${PREFIX}/lib/libcspice.so
 fi
+ln -s ${PREFIX}/lib/${CSPICENM} ${PREFIX}/lib/cspice.a
+ln -s ${PREFIX}/lib/${CSUPPTNM} ${PREFIX}/lib/csupport.a
