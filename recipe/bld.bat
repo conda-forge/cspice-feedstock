319f82920b980d5a08a1d882590b5f14e0eb64b6
Andrew Annex <ama6fy@virginia.edu>
2020-12-07 18:56:31 -0500

    add functionality to rebuild all of cspice from scratch for windows, include exe's for windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,18 +1,35 @@
 mkdir %LIBRARY_INC%\cspice
 
+rem copy needed files to cspice source directory
 copy "%RECIPE_DIR%\\makeDynamicSpice.bat" %SRC_DIR%\src\cspice
 copy "%RECIPE_DIR%\\cspice.def" %SRC_DIR%\src\cspice
 
 cd %SRC_DIR%\src\cspice
 
 call makeDynamicSpice.bat
+if errorlevel 1 exit 1
 
 copy "cspice.dll" %LIBRARY_BIN%
+if errorlevel 1 exit 1
 
+copy "cspice.dll" %LIBRARY_LIB%
 if errorlevel 1 exit 1
 
 cd %SRC_DIR%
 
 copy "include\\*.h" %LIBRARY_INC%\cspice
+if errorlevel 1 exit 1
 
+rem now that we have built the shared library we can do a full rebuild of cspice
+call makeall.bat
+if errorlevel 1 exit 1
+
+rem now copy static libraries to location they need to be in to get into path
+copy lib\cspice.lib %LIBRARY_LIB%\
+if errorlevel 1 exit 1
+copy lib\cupport.lib %LIBRARY_LIB%\
+if errorlevel 1 exit 1
+
+rem now copy executables to location they need to be in to get into path
+copy "exe\*"  %LIBRARY_BIN%\
 if errorlevel 1 exit 1
